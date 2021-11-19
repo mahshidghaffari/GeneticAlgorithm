86f7ccb960d33a6119fc61e8b35bbe7a5dcb47c5
mahshidghaffari <m.ghaffari@student.maastrichtuniversity.nl>
2021-11-19 11:50:50 +0100

    fix dublicate paranet

--- code/GA.java
+++ code/GA.java
@@ -1,28 +1,27 @@
 import java.util.ArrayList;
 import java.util.Random;
 
-public class GA{
+public class GA {
 
 	private Individual[] population;
 	private final String TARGET = "HELLO WORLD";
 	private char[] alphabet = new char[26];
 	private Random generator = new Random(System.currentTimeMillis());
 
-	private int popSize;  //population size
-	private double mutationRate;  //Probability of mutation to happen 
-
+	private int popSize; // population size
+	private double mutationRate; // Probability of mutation to happen
 
 	// initial the population
-	public GA (int popSize, double mutationRate){
+	public GA(int popSize, double mutationRate) {
 		this.popSize = popSize;
 		this.mutationRate = mutationRate;
 		this.population = new Individual[popSize];
-		this.alphabetMaker();   // make alphabet array
-		this.popGenerator(); 	// initial population
+		this.alphabetMaker(); // make alphabet array
+		this.popGenerator(); // initial population
 	}
 
-	//make an array of charecter with all alphabet
-	public void alphabetMaker(){
+	// make an array of charecter with all alphabet
+	public void alphabetMaker() {
 		// make an array with all the alphabet
 		for (char c = 'A'; c <= 'Z'; c++) {
 			alphabet[c - 'A'] = c;
@@ -30,7 +29,7 @@ public class GA{
 	}
 
 	// make an array of population (String of length (target))
-	public void popGenerator(){
+	public void popGenerator() {
 		for (int i = 0; i < popSize; i++) {
 			char[] tempChromosome = new char[TARGET.length()];
 			for (int j = 0; j < TARGET.length(); j++) {
@@ -40,29 +39,29 @@ public class GA{
 		}
 	}
 
-	public void printPop(){
+	public void printPop() {
 		for (int i = 0; i < population.length; i++) {
 			System.out.println(population[i].genoToPhenotype());
 		}
 	}
 
 	// assign a value to each phenotype according :
-		// if the charecter exist in the target  +1
-		//else if the charecter is same with the same index +100
-		// make an array of fitness value with the same indexing 
+	// if the charecter exist in the target +1
+	// else if the charecter is same with the same index +100
+	// make an array of fitness value with the same indexing
 	public void setFittness() {
 		for (int i = 0; i < population.length; i++) {
 
-			String phenotype = population[i].genoToPhenotype(); 
-			String tempTarget = TARGET; 
+			String phenotype = population[i].genoToPhenotype();
+			String tempTarget = TARGET;
 			int fitnessValue = 0;
 			for (int j = 0; j < phenotype.length(); j++) {
 
 				String gene = String.valueOf(phenotype.charAt(j));
-				if(tempTarget.contains(gene)){
+				if (tempTarget.contains(gene)) {
 					fitnessValue += 1;
-					if (tempTarget.indexOf(gene) == j){
-						fitnessValue +=100;
+					if (tempTarget.indexOf(gene) == j) {
+						fitnessValue += 100;
 					}
 					tempTarget.replace(gene, "#");
 
@@ -72,12 +71,10 @@ public class GA{
 			population[i].setFitness(fitnessValue);
 		}
 
-
 	}
 
-
 	// return total fittnes value of population
-	public double totalFittnessPop(){
+	public double totalFittnessPop() {
 		double total = 0;
 		for (int i = 0; i < population.length; i++) {
 			total += population[i].getFitness();
@@ -87,39 +84,46 @@ public class GA{
 
 	// pick two phenotype randomly base on their distributions
 
-	public ArrayList<Individual> naturalSelection(){
+	public ArrayList<Individual> naturalSelection() {
 		ArrayList<Integer> probDistributionPop = new ArrayList<Integer>();
-		Individual firstSelected; 
-		Individual secondSelected; 
+		Individual firstSelected;
+		Individual secondSelected;
 		Random rndOne = new Random();
 		Random rndTwo = new Random();
-		ArrayList<Individual> selectedParents= new ArrayList<Individual>();
+		ArrayList<Individual> selectedParents = new ArrayList<Individual>();
 
 		// make and array of phenotype based in their distribution
-		for (int i = 0 ; i< this.population.length;i++){
-			for(int j = 0; j<population[i].getFitness();j++){
+		for (int i = 0; i < this.population.length; i++) {
+			for (int j = 0; j < population[i].getFitness(); j++) {
 				probDistributionPop.add(i);
 			}
 		}
 
-
+		System.out.println(probDistributionPop.toString());
 		// select two Individual randomly from probDistributionPop
-		int rndOneInt = rndOne.nextInt(probDistributionPop.size()); 
-		int rndTwoInt = rndTwo.nextInt(probDistributionPop.size()); 
+		int rndOneInt = rndOne.nextInt(probDistributionPop.size());
+		int rndTwoInt = rndTwo.nextInt(probDistributionPop.size());
 
 		int indexFistSelected = probDistributionPop.get(rndOneInt);
 		int indexSecondSelected = probDistributionPop.get(rndTwoInt);
-		
+
 		firstSelected = population[indexFistSelected];
 		secondSelected = population[indexSecondSelected];
+
+		// this while is for avoiding having the same parent
+		while (indexFistSelected == indexSecondSelected) {
+			int tempRnd = rndTwo.nextInt(probDistributionPop.size());
+			indexSecondSelected = probDistributionPop.get(tempRnd);
+			secondSelected = population[indexSecondSelected];
+		}
+
 		selectedParents.add(firstSelected);
 		selectedParents.add(secondSelected);
-		
+
 		System.out.println(selectedParents.get(0).genoToPhenotype());
 		System.out.println(selectedParents.get(1).genoToPhenotype());
-
 		return selectedParents;
-		
+
 	}
 
 }
--- code/main.java
+++ code/main.java
@@ -6,7 +6,7 @@ public class main {
     public static String man = "mahshid";
 
     public static void main(String[] args) {
-        GA test = new GA(10, 1.00);
+        GA test = new GA(5, 1.00);
         test.printPop();
         test.setFittness();
         test.naturalSelection();
